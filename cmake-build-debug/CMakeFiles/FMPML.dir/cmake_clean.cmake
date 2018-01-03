99f2e959b1266da56ced6f268321732fbc42691a
NotLebedev <artem-itf@yandex.ru>
2018-01-03 15:51:18 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/FMPML.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FMPML.dir/library.cpp.o"
+  "cygFMPML.pdb"
+  "cygFMPML.dll"
+  "libFMPML.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FMPML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ library.h
@@ -0,0 +1,6 @@
+#ifndef FMPML_LIBRARY_H
+#define FMPML_LIBRARY_H
+
+void hello();
+
+#endif
